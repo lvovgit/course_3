e80fbdcd293d0704fcb32e95604a5b1f8ccd0eeb
lvovgit <romka.lvov@gmail.com>
2023-02-11 11:46:34 +0300

    change url

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,7 +2,7 @@ import pytest
 
 @pytest.fixture()
 def test_url():
-    return "https://s3.us-west-2.amazonaws.com/secure.notion-static.com/d22c7143-d55e-4f1d-aa98-e9b15e5e5efc/operations.json?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20230209%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20230209T182940Z&X-Amz-Expires=86400&X-Amz-Signature=6ce43917e1e4c8d87892a044cdc564236b7ac1b3a860cfdf4e2a2512ca74df78&X-Amz-SignedHeaders=host&response-content-disposition=filename%3D%22operations.json%22&x-id=GetObject"
+    return "https://file.notion.so/f/s/d22c7143-d55e-4f1d-aa98-e9b15e5e5efc/operations.json?spaceId=0771f0bb-b4cb-4a14-bc05-94cbd33fc70d&table=block&id=f11058ed-10ad-42ea-a13d-aad1945e5421&expirationTimestamp=1676191139486&signature=k-NvkhBqIR4M4PTNCoqoclGQYO8sptP9Nz6i2PcMXQI&downloadName=operations.json"
 
 @pytest.fixture
 def test_data():
